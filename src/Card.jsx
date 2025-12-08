cd6b69114caee537ff017736adb98bd24ff2c286
JaronOris <jaronoris@protonmail.com>
2025-12-08 01:06:20 -0500

    I have no idea what i'm doing

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,60 +1,63 @@
-// import React, { useState, useEffect, useRef } from "react";
-//
-// const [isRunning, setIsRunning] = useState(false);
-// const [allottedTime, setAllottedTime] = useState(0);
-// const [startingTime, setStartingtime] = useState(0);
-// const intervalIdRef = useRef(null);
-// const startTimeRef = useRef(0);
-//
-// useEffect(() => {
-//
-// }, [isRunning]);
-//
-// function handleInputChange(event) {
-//   setAllottedTime(event.target.value)
-// };
-//
-// function getAllotted() { };
-//
-// function startingValue() { };
-//
-// function start() {
-//   console.log(allottedTime)
-// };
-//
-// function stop() { };
-//
-// function reset() { };
-//
-function formatTime() {
-  return `00:00:00`;
-}
+import React, { useRef, useState, useEffect } from "react";
 
 function Card() {
+
+  const [stopTime, setStopTime] = useState();
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const allottedTime = useRef(null);
+  const startTime = useRef(null);
+
+
+  function timerStart() {
+
+    const timeLimit = +allottedTime.current.value;
+    let start = startTime.current.value;
+
+    const hourLimit = +start.slice(0, start.indexOf(":"));
+    const minuteLimit = +start.slice(start.indexOf(":") +1)
+
+    const allottHour = Math.floor(timeLimit / 60);
+    const allottMinute = Math.floor(timeLimit %60);
+
+    let timeRemainHour = allottHour + hourLimit; 
+    const timeRemainMin = allottMinute + minuteLimit;
+
+    timeRemainHour = timeRemainHour % 12 || 12;
+  
+    setStopTime(timeRemainHour + ":" + timeRemainMin)
+  }
+
+  function formatTime() {
+    if (stopTime == undefined) {
+      return `00:00`
+    } else {
+      return `${stopTime}`
+    }
+  }
+
   return (
     <div className="card">
       <div className="timeInfo">
         <div className="timeBlock">
           <form>
-            <label for="allottedValue">Allotted Time</label>
+            <label htmlFor="allottedValue">Allotted Time</label>
             <input
+              ref={allottedTime}
               type="text"
               name="allottedValue"
-              maxlength="8"
-              value=""
-              onChange=""
+              maxLength="3"
             />
           </form>
         </div>
         <div className="timeBlock">
           <form>
-            <label for="startValue">Start Time</label>
+            <label htmlFor="startValue">Start Time</label>
             <input
+              ref={startTime}
               type="text"
               name="startValue"
-              maxlength="8"
-              value=""
-              onChange=""
+              maxLength="5"
             />
           </form>
         </div>
@@ -63,13 +66,13 @@ function Card() {
         <div className="display">{formatTime()}</div>
       </div>
       <div className="controls">
-        <button onClick="" className="start button">
+        <button onClick={timerStart} className="start button">
           Start
         </button>
-        <button onClick="" className="stop button">
+        <button className="stop button">
           Stop
         </button>
-        <button onClick="" className="reset button">
+        <button className="reset button">
           Reset
         </button>
       </div>
